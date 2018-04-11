09b1ad15f547f6991be630071205b030e937b19d
Schwarzer <wangzwnj@126.com>
2018-04-11 22:23:04 +0800

    Project Christine

---
+++ UABESharpTester/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UABESharp;
+using System.IO;
+
+namespace UABESharpTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] b = File.ReadAllBytes(@"resources.assets");
+            int Reader =  AssetsFileReader.CreateAssetsReaderFromMemory(b, (uint)b.Length, true);
+            AssetsFile assetsFile = new AssetsFile(AssetsFileReader.AssetsReaderFromMemory, Reader);
+            AssetsFileTable assetsFileTable = new AssetsFileTable(assetsFile, true);
+            for (int i = 0; i < assetsFileTable.AssetFileInfoCount; ++i)
+            {
+                AssetFileInfoEx Asset = assetsFileTable.GetAssetInfo((ulong)i);             
+                if (Asset == null) continue;
+                Console.WriteLine(Asset.Name);
+            }
+        }
+    }
+}
